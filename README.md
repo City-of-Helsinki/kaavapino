0792591b7fbb7685fb54984dfdc2e8131fbb885f
Tuomas Haapala <tuomas.haapala@anders.fi>
2017-12-13 11:32:45 +0200

    Project boilerplate
    Closes #3

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
 # kaavapino
 Project management system for city planning projects.
+
+## Prerequisites
+
+* PostgreSQL (>= 9.3)
+* Python (>= 3.4)
+
+## Development
+
+### Install required system packages
+
+#### PostgreSQL and PostGIS
+
+Install PostgreSQL and PostGIS.
+
+    # Ubuntu 16.04
+    sudo apt-get install python3-dev libpq-dev postgresql postgis
+
+#### GeoDjango extra packages
+
+    # Ubuntu 16.04
+    sudo apt-get install binutils libproj-dev gdal-bin
+
+### Creating a Python virtualenv
+
+Create a Python 3.x virtualenv either using the [`venv`](https://docs.python.org/3/library/venv.html) tool or using
+the great [virtualenvwrapper](https://virtualenvwrapper.readthedocs.io/en/latest/) toolset. Assuming the latter,
+once installed, simply do:
+
+    mkvirtualenv -p /usr/bin/python3 kaavapino
+
+The virtualenv will automatically activate. To activate it in the future, just do:
+
+    workon kaavapino
+
+### Creating Python requirements files
+
+* Run `prequ compile`
+
+### Updating Python requirements files
+
+* Run `prequ update`
+
+### Installing Python requirements
+
+* Run `prequ sync`
+* For development run `prequ sync requirements.txt requirements-dev.txt`
+
+### Database
+
+To setup a database compatible with the default database settings:
+
+Create user and database
+
+    sudo -u postgres createuser -P -R -S kaavapino  # use password `kaavapino`
+    sudo -u postgres createdb -O kaavapino kaavapino
+
+Enable PostGIS
+
+    sudo -u postgres psql -d "kaavapino" -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+
+Allow the kaavapino user to create databases when running tests
+
+    sudo -u postgres psql -c "ALTER USER kaavapino CREATEDB;"
+
+Tests also require that PostGIS extension is installed on the test database. This can be achieved most easily by
+adding PostGIS extension to the default template which is then used when the test databases are created:
+
+    sudo -u postgres psql -d template1 -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+
+### Django configuration
+
+Environment variables are used to customize configuration in `kaavapino/settings.py`. If you wish to override any
+settings, you can place them in a local `.env` file which will automatically be sourced when Django imports
+the settings file.
+
+Alternatively you can create a `local_settings.py` which is executed at the end of the `kaavapino/settings.py` in the
+same context so that the variables defined in the settings are available.
+
+### Running development environment
+
+* Enable debug `echo 'DEBUG=True' >> .env`
+* Run `python manage.py migrate`
+* Run `python manage.py runserver 0.0.0.0:8000`
+
+## Running tests
+
+* Run `pytest`
---
+++ kaavapino/settings.py
@@ -0,0 +1,110 @@
+"""
+Django settings for kaavapino project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.11/ref/settings/
+"""
+
+import os
+
+import environ
+
+project_root = environ.Path(__file__) - 2
+
+env = environ.Env(
+    DEBUG=(bool, True),
+    SECRET_KEY=(str, ''),
+    ALLOWED_HOSTS=(list, []),
+    DATABASE_URL=(str, 'postgis://kaavapino:kaavapino@localhost/kaavapino'),
+    CACHE_URL=(str, 'locmemcache://'),
+    EMAIL_URL=(str, 'consolemail://'),
+    MEDIA_ROOT=(environ.Path, project_root('media')),
+    STATIC_ROOT=(environ.Path, project_root('static')),
+    MEDIA_URL=(str, '/media/'),
+    STATIC_URL=(str, '/static/'),
+)
+
+env_file = project_root('.env')
+
+if os.path.exists(env_file):
+    env.read_env(env_file)
+
+DEBUG = env.bool('DEBUG')
+SECRET_KEY = env.str('SECRET_KEY')
+
+if DEBUG and not SECRET_KEY:
+    SECRET_KEY = 'xxx'
+
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+
+DATABASES = {
+    'default': env.db()
+}
+
+CACHES = {
+    'default': env.cache()
+}
+
+vars().update(env.email_url())  # EMAIL_BACKEND etc.
+
+STATIC_URL = env.str('STATIC_URL')
+MEDIA_URL = env.str('MEDIA_URL')
+STATIC_ROOT = str(env.path('STATIC_ROOT'))
+MEDIA_ROOT = str(env.path('MEDIA_ROOT'))
+
+ROOT_URLCONF = 'kaavapino.urls'
+WSGI_APPLICATION = 'kaavapino.wsgi.application'
+
+LANGUAGE_CODE = 'fi'
+TIME_ZONE = 'Europe/Helsinki'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'plans',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
+local_settings = project_root('local_settings.py')
+if os.path.exists(local_settings):
+    with open(local_settings) as fp:
+        code = compile(fp.read(), local_settings, 'exec')
+    exec(code, globals(), locals())
