de94195e7ce62055a4dfe2e82056e4f02b328a75
Tuomas Haapala <tuomas.haapala@anders.fi>
2018-01-08 15:15:29 +0200

    Remove development index view

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,5 @@
 import random
 import json
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, UpdateView
@@ -8,25 +7,6 @@ from django.views.generic.list import ListView
 
 from .forms import ProjectForm
 from .models import Attribute, Project
-
-
-def index(request, path='index'):
-    if 'favicon.ico' in path:
-        return HttpResponse('')
-
-    template_filename = '{}.html'.format(path)
-    project_qs = Project.objects.filter(geometry__isnull=False, phase__isnull=False)
-    project_qs = project_qs.select_related('phase')
-    strategy_attr = Attribute.objects.get(identifier='strategiakytkenta')
-    strategies = {x.identifier: x for x in strategy_attr.value_choices.all()}
-    for project in project_qs:
-        project.sqm2 = random.randint(1, 250)
-        project_strategies = project.attribute_data.get('strategiakytkenta', [])
-        project.strategies = json.dumps([strategies[x].value for x in project_strategies])
-    context = dict(projects=project_qs)
-
-    return render(request, template_filename, context=context)
-
 
 
 class ProjectCreateView(CreateView):
@@ -51,3 +31,17 @@ class ProjectUpdateView(UpdateView):
 class ProjectListView(ListView):
     model = Project
     template_name = 'project_list.html'
+
+
+def report_view(request):
+    project_qs = Project.objects.filter(geometry__isnull=False, phase__isnull=False)
+    project_qs = project_qs.select_related('phase')
+    strategy_attr = Attribute.objects.get(identifier='strategiakytkenta')
+    strategies = {x.identifier: x for x in strategy_attr.value_choices.all()}
+    for project in project_qs:
+        project.sqm2 = random.randint(1, 250)
+        project_strategies = project.attribute_data.get('strategiakytkenta', [])
+        project.strategies = json.dumps([strategies[x].value for x in project_strategies])
+    context = dict(projects=project_qs)
+
+    return render(request, 'report.html', context=context)
