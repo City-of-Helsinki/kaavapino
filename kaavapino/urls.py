86975f54c5cec44c5992661cffb9a0df949e00d2
Mikko Keskinen <mikko.keskinen@anders.fi>
2018-01-25 14:24:14 +0200

    Sort imports

--- kaavapino/urls.py
+++ kaavapino/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path, include
+from django.urls import include, path
 from django.views.generic import TemplateView
 
 from projects import views as projects_views
