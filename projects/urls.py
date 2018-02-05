07795671c53cc959e24de86f561fbd638280ceaa
Mikko Keskinen <mikko.keskinen@anders.fi>
2018-02-05 14:49:42 +0200

    Add ability to change phase

--- projects/urls.py
+++ projects/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('<int:pk>/', views.ProjectCardView.as_view(), name='card'),
     path('<int:pk>/edit/', views.project_edit, name='edit'),
     path('<int:pk>/edit/phase/<int:phase_id>', views.project_edit, name='edit-phase'),
+    path('<int:pk>/change-phase/', views.project_change_phase, name='change-phase'),
     path('<int:pk>/create-document/', views.DocumentCreateView.as_view(), name='document-create'),
     path('<int:project_pk>/create-document/<int:document_pk>/', views.document_download_view, name='document-download'),
 ]
