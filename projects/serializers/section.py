ef8cbdb14c4dd7bd7f072865d005dfcd3a01bae9
Frank Wickström <frank.wickstrom@anders.fi>
2018-10-24 09:53:22 +0300

    Refactor multipurpose utility serializer functions to its own file

--- projects/serializers/section.py
+++ projects/serializers/section.py
@@ -1,59 +1,9 @@
-from collections import namedtuple
-
-from django.contrib.auth import get_user_model
-from django.forms import DateInput
 from rest_framework import serializers
 
-from projects.models import Attribute
-
-
-class DateInputType(DateInput):
-    input_type = "date"
-
-
-FIELD_TYPES = {
-    Attribute.TYPE_SHORT_STRING: serializers.CharField,
-    Attribute.TYPE_LONG_STRING: serializers.CharField,
-    Attribute.TYPE_INTEGER: serializers.IntegerField,
-    Attribute.TYPE_BOOLEAN: serializers.BooleanField,
-    Attribute.TYPE_DATE: serializers.DateField,
-    Attribute.TYPE_IMAGE: serializers.ImageField,  # TODO: Figure out file uploads with DRF
-    Attribute.TYPE_USER: serializers.PrimaryKeyRelatedField,
-    # TODO Add Attribute.TYPE_GEOMETRY
-}
-
-FieldData = namedtuple("FieldData", ["field_class", "field_arguments"])
-
-
-def _get_serializer_field_data(attribute):
-    field_arguments = {}
-    field_class = FIELD_TYPES.get(attribute.value_type, None)
-
-    choices = attribute.value_choices.all()
-    if choices:
-        field_arguments["choices"] = choices
-
-        if attribute.multiple_choice:
-            field_class = serializers.MultipleChoiceField
-
-    if attribute.value_type == Attribute.TYPE_USER:
-        field_arguments["queryset"] = get_user_model().objects.all()
-
-    field_arguments["help_text"] = attribute.help_text
-
-    return FieldData(field_class, field_arguments)
-
-
-def _is_attribute_required(section_attribute):
-    attribute = section_attribute.attribute
-
-    if (
-        attribute.value_type != Attribute.TYPE_BOOLEAN
-        and not section_attribute.generated
-    ):
-        return section_attribute.required
-    else:
-        return False
+from projects.serializers.utils import (
+    _get_serializer_field_data,
+    _is_attribute_required,
+)
 
 
 def create_section_serializer(section):
---
+++ projects/tests/test_utils.py
@@ -0,0 +1,69 @@
+import pytest
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from projects.models import Attribute, AttributeValueChoice
+from projects.serializers.utils import (
+    _is_attribute_required,
+    _get_serializer_field_data,
+)
+
+
+@pytest.mark.django_db()
+def test_is_attribute_required(f_project_section_attribute_1):
+    required = _is_attribute_required(f_project_section_attribute_1)
+    assert required is False
+
+    # required = True
+    f_project_section_attribute_1.required = True
+    required = _is_attribute_required(f_project_section_attribute_1)
+    assert required is True
+
+    # required = True, generated = True
+    f_project_section_attribute_1.generated = True
+    required = _is_attribute_required(f_project_section_attribute_1)
+    assert required is False
+
+    # required = True, generated = False, Boolean field
+    f_project_section_attribute_1.generated = False
+    f_project_section_attribute_1.attribute.value_type = Attribute.TYPE_BOOLEAN
+    required = _is_attribute_required(f_project_section_attribute_1)
+    assert required is False
+
+
+@pytest.mark.django_db()
+def test_get_serializer_field_data(
+    f_short_string_attribute,
+    f_user_attribute,
+    f_short_string_multi_choice_attribute,
+    f_short_string_choice_attribute,
+):
+    short_string_field = _get_serializer_field_data(f_short_string_attribute)
+    user_field = _get_serializer_field_data(f_user_attribute)
+    multi_choice_field = _get_serializer_field_data(
+        f_short_string_multi_choice_attribute
+    )
+    choice_field = _get_serializer_field_data(f_short_string_choice_attribute)
+
+    fields = [short_string_field, user_field, multi_choice_field, choice_field]
+    choices_field = [multi_choice_field, choice_field]
+
+    assert short_string_field.field_class == serializers.CharField
+    assert user_field.field_class == serializers.PrimaryKeyRelatedField
+    assert multi_choice_field.field_class == serializers.MultipleChoiceField
+    assert choice_field.field_class == serializers.CharField
+
+    for field in fields:
+        assert "help_text" in field.field_arguments
+
+    for choice_field in choices_field:
+        assert len(choice_field.field_arguments["choices"]) > 0
+        assert (
+            type(choice_field.field_arguments["choices"].first())
+            == AttributeValueChoice
+        )
+
+    assert len(user_field.field_arguments["queryset"]) == len(
+        get_user_model().objects.all()
+    )
+    assert type(user_field.field_arguments["queryset"].first()) == get_user_model()
