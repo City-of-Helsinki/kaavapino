7b8a0851f309ae70f738e62b53ef6eddfc63c896
Juha Louhiranta <juha.louhiranta@anders.fi>
2018-10-31 10:13:50 +0200

    Add test for importer
    Test that correct amount of phases are created

---
+++ projects/tests/test_importer.py
@@ -0,0 +1,16 @@
+import pytest
+
+from projects.importing import AttributeImporter
+from projects.models import ProjectPhase
+
+
+@pytest.mark.django_db()
+def test_project_phases_are_created(f_project_type):
+    ai = AttributeImporter()
+    ai.project_type = f_project_type
+    ai.create_phases()
+
+    assert ProjectPhase.objects.all().count() == 6
+
+    for i in range(1, 7):
+        assert ProjectPhase.objects.filter(index=i).exists()
