5d00f38034ed87a9c1dbfd919f53170c073c3200
Juha Louhiranta <juha.louhiranta@anders.fi>
2018-11-12 14:43:48 +0200

    Add factory-boy factories for various models

--- projects/tests/conftest.py
+++ projects/tests/conftest.py
@@ -1,3 +1,16 @@
+from pytest_factoryboy import register
+
 # Disable Flake8 checks here as having fixtures in their
 # own file is a lot nicer for readability.
+from users.tests.factories import UserFactory
 from .fixtures import *  # noqa: F401,F403
+from .factories import *  # noqa: F403
+
+register(UserFactory)
+register(AttributeFactory)
+register(FieldSetAttributeFactory)
+register(ProjectTypeFactory)
+register(ProjectPhaseFactory)
+register(ProjectPhaseSectionFactory)
+register(ProjectPhaseSectionAttributeFactory)
+register(ProjectFactory)
---
+++ projects/tests/factories.py
@@ -0,0 +1,88 @@
+import factory
+from factory import fuzzy
+
+from projects.models import (
+    Attribute,
+    Project,
+    ProjectType,
+    ProjectPhase,
+    ProjectPhaseSection,
+    ProjectPhaseSectionAttribute,
+)
+from projects.models.attribute import FieldSetAttribute
+from users.tests.factories import UserFactory
+
+__all__ = (
+    "AttributeFactory",
+    "FieldSetAttributeFactory",
+    "ProjectTypeFactory",
+    "ProjectPhaseFactory",
+    "ProjectPhaseSectionFactory",
+    "ProjectPhaseSectionAttributeFactory",
+    "ProjectFactory",
+)
+
+
+class AttributeFactory(factory.DjangoModelFactory):
+    name = fuzzy.FuzzyText(prefix="Attribute ", length=16)
+    help_text = factory.Sequence(lambda n: f"Help for attribute {n}")
+    value_type = Attribute.TYPE_SHORT_STRING
+    identifier = fuzzy.FuzzyText(prefix="attribute", length=16)
+
+    class Meta:
+        model = Attribute
+
+
+class FieldSetAttributeFactory(factory.DjangoModelFactory):
+    attribute_source = factory.SubFactory(AttributeFactory)
+    attribute_target = factory.SubFactory(AttributeFactory)
+    index = factory.Sequence(int)
+
+    class Meta:
+        model = FieldSetAttribute
+
+
+class ProjectTypeFactory(factory.DjangoModelFactory):
+    name = "asemakaava"
+
+    class Meta:
+        model = ProjectType
+        django_get_or_create = ("name",)
+
+
+class ProjectPhaseFactory(factory.DjangoModelFactory):
+    project_type = factory.SubFactory(ProjectTypeFactory)
+    name = fuzzy.FuzzyText()
+    index = factory.Sequence(int)
+
+    class Meta:
+        model = ProjectPhase
+
+
+class ProjectPhaseSectionFactory(factory.DjangoModelFactory):
+    phase = factory.SubFactory(ProjectPhaseFactory)
+    name = fuzzy.FuzzyText()
+    index = factory.Sequence(int)
+
+    class Meta:
+        model = ProjectPhaseSection
+
+
+class ProjectPhaseSectionAttributeFactory(factory.DjangoModelFactory):
+    attribute = factory.SubFactory(AttributeFactory)
+    section = factory.SubFactory(ProjectPhaseSectionFactory)
+    index = factory.Sequence(int)
+
+    class Meta:
+        model = ProjectPhaseSectionAttribute
+
+
+class ProjectFactory(factory.DjangoModelFactory):
+    identifier = fuzzy.FuzzyText()
+    name = factory.Faker("street_name")
+    user = factory.SubFactory(UserFactory)
+    type = factory.SubFactory(ProjectTypeFactory)
+    phase = factory.SubFactory(ProjectPhaseFactory)
+
+    class Meta:
+        model = Project
