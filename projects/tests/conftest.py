b34bcca1b549cb78a98d8b546d87cc737ced77d5
Juha Louhiranta <juha.louhiranta@anders.fi>
2018-11-13 00:19:16 +0200

    Make flake8 ignore conftest

--- projects/tests/conftest.py
+++ projects/tests/conftest.py
@@ -1,10 +1,11 @@
+# flake8: noqa
+# Disable Flake8 checks here as having fixtures and factories in their
+# own files is a lot nicer for readability.
 from pytest_factoryboy import register
 
-# Disable Flake8 checks here as having fixtures in their
-# own file is a lot nicer for readability.
 from users.tests.factories import UserFactory
-from .fixtures import *  # noqa: F401,F403
-from .factories import *  # noqa: F403
+from .fixtures import *
+from .factories import *
 
 register(UserFactory)
 register(AttributeFactory)
