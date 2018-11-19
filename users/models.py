1a756705289bf5b339544fca4413913483c04f1b
Frank Wickström <frank.wickstrom@anders.fi>
2018-11-19 15:35:07 +0200

    Add user group helper function

--- users/models.py
+++ users/models.py
@@ -1,5 +1,22 @@
+from django.contrib.auth.models import Group
 from helusers.models import AbstractUser
+
+from users.groups import GROUPS
 
 
 class User(AbstractUser):
-    pass
+    def is_in_group(self, group):
+        if isinstance(group, Group):
+            return self.groups.filter(group=group).exists()
+        return self.groups.filter(name=group).exists()
+
+    def is_in_any_of_groups(self, groups):
+        if not groups:
+            return False
+
+        if isinstance(groups[0], Group):
+            return self.groups.filter(group__in=groups).exists()
+        return self.groups.filter(name__in=groups).exists()
+
+    def is_administrative_personnel(self):
+        return self.is_in_any_of_groups(GROUPS.ADMINISTRATIVE_PERSONNEL)
