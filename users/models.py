647f7ba683cff405f3ca0b060246329c659515d3
Frank Wickström <frank.wickstrom@anders.fi>
2018-11-19 15:35:07 +0200

    Add is_in_group user helper function

--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.models import Group
 from helusers.models import AbstractUser
 
 
 class User(AbstractUser):
-    pass
+    def is_in_group(self, group):
+        if isinstance(group, Group):
+            return self.groups.filter(group=group).exists()
+        return self.groups.filter(name=group).exists()
