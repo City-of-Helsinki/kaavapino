f1f08995e3f68e4163842245300cc64d7a73e84b
Frank Wickström <frank.wickstrom@anders.fi>
2018-11-19 11:09:38 +0200

    Use a hybrid JSON/Method field for attribute data
    JSON Field -> Write
    Method field + JSON Field -> Read

---
+++ projects/serializers/fields.py
@@ -0,0 +1,26 @@
+from rest_framework.fields import JSONField
+
+
+class AttributeDataField(JSONField):
+    def __init__(self, method_name=None, **kwargs):
+        self.method_name = method_name
+        super().__init__(**kwargs)
+
+    def bind(self, field_name, parent):
+        default_method_name = "get_{field_name}".format(field_name=field_name)
+        assert self.method_name != default_method_name, (
+            "It is redundant to specify `%s` on SerializerMethodField '%s' in "
+            "serializer '%s', because it is the same as the default method name. "
+            "Remove the `method_name` argument."
+            % (self.method_name, field_name, parent.__class__.__name__)
+        )
+
+        # The method name should default to `get_{field_name}`.
+        if self.method_name is None:
+            self.method_name = default_method_name
+
+        super().bind(field_name, parent)
+
+    def to_representation(self, value):
+        method = getattr(self.parent, self.method_name)
+        return super().to_representation(method(value))
