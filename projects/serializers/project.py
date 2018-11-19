20b25383552a5265ae230eb9a89de13e901fe79a
Frank Wickström <frank.wickstrom@anders.fi>
2018-11-19 14:27:02 +0200

    Always set file attributes
    For this to work we also need to make sure that the default value of the
    attributes always is a dict even if the attribute does not exist.

--- projects/serializers/project.py
+++ projects/serializers/project.py
@@ -53,9 +53,8 @@ class ProjectSerializer(serializers.ModelSerializer):
         read_only_fields = ["phase", "type", "created_at", "modified_at"]
 
     def get_attribute_data(self, project):
-        attribute_data = getattr(project, "attribute_data", None)
-        if attribute_data:
-            self._set_file_attributes(attribute_data, project)
+        attribute_data = getattr(project, "attribute_data", {})
+        self._set_file_attributes(attribute_data, project)
 
         return attribute_data
 
