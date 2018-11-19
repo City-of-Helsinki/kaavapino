0f7741b209ec7f8c89c4555f2d9d209fb593b4bb
Frank Wickström <frank.wickstrom@anders.fi>
2018-11-19 15:47:53 +0200

    fixup! Add validation for phase and user attributes

--- projects/validators.py
+++ projects/validators.py
@@ -1,8 +1,6 @@
 from django.utils.translation import ugettext_lazy as _
 from rest_framework.exceptions import ValidationError
 
-from users.groups import GROUPS
-
 
 def admin_or_read_only(value, attribute, instance, context):
     user = context["request"].user
@@ -12,7 +10,7 @@ def admin_or_read_only(value, attribute, instance, context):
         return value
 
     # Do not allow non admins to change the value
-    is_admin = user.is_in_group(GROUPS.ADMINISTRATOR)
+    is_admin = user.is_administrative_personnel()
     if not is_admin:
         raise ValidationError(
             _(f"You are not permitted to change the {attribute} of this project")
