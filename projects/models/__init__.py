3ea5829475c8eca1c0cd5c2cc3d4f1f8cc7ccda7
Juha Louhiranta <juha.louhiranta@anders.fi>
2018-11-22 01:48:04 +0200

    Add model for project related comment

--- projects/models/__init__.py
+++ projects/models/__init__.py
@@ -1,4 +1,5 @@
 from .attribute import Attribute, AttributeValueChoice, FieldSetAttribute  # noqa
+from .projectcomment import ProjectComment  # noqa
 from .document import DocumentTemplate  # noqa
 from .project import (  # noqa
     Project,
