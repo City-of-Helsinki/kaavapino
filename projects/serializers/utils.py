491a844464ca60e4b719c15eee284823b01bce7d
Frank Wickström <frank.wickstrom@anders.fi>
2018-12-04 14:55:56 +0200

    Let boolean fields be required if set in the attribute instance

--- projects/serializers/utils.py
+++ projects/serializers/utils.py
@@ -2,7 +2,7 @@ from projects.models import Attribute
 
 
 def _is_attribute_required(attribute: Attribute):
-    if attribute.value_type != Attribute.TYPE_BOOLEAN and not attribute.generated:
+    if not attribute.generated:
         return attribute.required
     else:
         return False
