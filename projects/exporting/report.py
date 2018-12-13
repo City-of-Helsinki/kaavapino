ea6ded60082a311f38a0d8517c68f31ec6733149
Juha Louhiranta <juha.louhiranta@anders.fi>
2018-12-13 02:38:05 +0200

    Report CSV exports attribute display values

--- projects/exporting/report.py
+++ projects/exporting/report.py
@@ -1,15 +1,20 @@
 import copy
 import csv
+import logging
 from collections import OrderedDict
 
 from projects.models import Report
 
+logger = logging.getLogger(__name__)
+
 
 def render_report_to_response(report: Report, projects, response):
     fieldnames = OrderedDict()
+    attributes = []
 
     for report_attr in report.report_attributes.all():
         attribute = report_attr.attribute
+        attributes.append(attribute)
         fieldnames[attribute.identifier] = attribute.name
 
     writer = csv.DictWriter(
@@ -22,6 +27,18 @@ def render_report_to_response(report: Report, projects, response):
     # Write data
     for project in projects:
         data = copy.deepcopy(project.attribute_data)
+
+        # Raw values into display values
+        for attribute in attributes:
+            if attribute.identifier in data:
+                try:
+                    data[attribute.identifier] = attribute.get_attribute_display(
+                        data[attribute.identifier]
+                    )
+                except Exception:
+                    logger.exception(
+                        f"Couldn't handle attribute {attribute} for project {project}."
+                    )
         writer.writerow(data)
 
     return response
