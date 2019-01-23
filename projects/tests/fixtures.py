ead4b31fd1b6a5a8fc9e241ac9c210495f0989c2
Frank Wickström <frank.wickstrom@anders.fi>
2019-01-23 14:46:39 +0200

    Fix black styling

--- projects/tests/fixtures.py
+++ projects/tests/fixtures.py
@@ -271,7 +271,7 @@ def f_project_section_attribute_6(
 @pytest.mark.django_db()
 def f_project_section_attribute_6_file(f_file_attribute, f_project_section_1):
     return ProjectPhaseSectionAttribute.objects.create(
-        attribute=f_file_attribute, section=f_project_section_1, index=6, priority=1,
+        attribute=f_file_attribute, section=f_project_section_1, index=6, priority=1
     )
 
 
