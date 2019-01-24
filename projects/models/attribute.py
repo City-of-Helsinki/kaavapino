9ab229972ece873f0c005803e4053f2c6d182e68
Frank Wickström <frank.wickstrom@anders.fi>
2019-01-24 15:08:08 +0200

    Add help link attribute to attribute model

--- projects/models/attribute.py
+++ projects/models/attribute.py
@@ -122,6 +122,7 @@ class Attribute(models.Model):
         through_fields=("attribute_source", "attribute_target"),
     )
     help_text = models.TextField(verbose_name=_("Help text"), blank=True)
+    help_link = models.URLField(verbose_name=_("Help link"), blank=True, null=True)
     broadcast_changes = models.BooleanField(default=False)
 
     objects = AttributeQuerySet.as_manager()
