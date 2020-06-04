40fc935fcaad5db35f98420807cdcb5c4dd79717
Nuppu Eskelinen <nuppu.eskelinen@nidos.fi>
2020-06-04 12:17:17 +0300

    Manage groups, AD mappings and privileges from one admin view

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,30 @@
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+from django.contrib.auth.models import Group
+
 from helusers.admin import admin
+from helusers.models import ADGroupMapping
 
-from .models import User
+from .models import User, GroupPrivilege
 
+
+admin.site.unregister(Group)
 
 @admin.register(User)
 class UserAdmin(UserAdmin):
     pass
+
+class GroupPrivilegeInline(admin.TabularInline):
+    model = GroupPrivilege
+    can_delete = False
+
+class ADGroupMappingInline(admin.TabularInline):
+    model = ADGroupMapping
+    extra = 0
+
+@admin.register(Group)
+class GroupAdmin(GroupAdmin):
+    exclude = ('permissions',)
+    inlines = [
+        ADGroupMappingInline,
+        GroupPrivilegeInline,
+    ]
