11352b2e989831240183a4f836e7dcd1efaac25e
Nuppu Eskelinen <nuppu.eskelinen@nidos.fi>
2020-06-04 12:16:40 +0300

    Add model for group privileges

--- users/models.py
+++ users/models.py
@@ -1,8 +1,18 @@
 from django.contrib.auth.models import Group
 from helusers.models import AbstractUser
+from django.db import models
 
 from users.groups import GROUPS
 
+# TODO: hard-coded for the MVP, migrate into a separate model when needed
+# Available privilege levels in ascending order
+PRIVILEGE_LEVELS = (
+    (None, 'Ei roolia'),
+    ('browse', 'Selaaja'),
+    ('edit', 'Asiantuntija'),
+    ('create', 'Vastuuhenkilö'),
+    ('admin', 'Pääkäyttäjä'),
+)
 
 class User(AbstractUser):
     def is_in_group(self, group):
@@ -21,3 +31,44 @@ class User(AbstractUser):
     def is_administrative_personnel(self):
         admin_groups = list(GROUPS.ADMINISTRATIVE_PERSONNEL.values.keys())
         return self.is_in_any_of_groups(admin_groups)
+
+    def get_privilege(self):
+        privileges = tuple(p for (p, _) in PRIVILEGE_LEVELS)
+        user_privilege = None
+
+        for group in self.groups.all():
+            try:
+                privilege_name = group.groupprivilege.privilege_level
+                group_privilege = privileges.index(privilege_name)
+
+                if group_privilege > privileges.index(user_privilege):
+                    user_privilege = privileges[group_privilege]
+
+            except AttributeError:
+                pass
+            except TypeError:
+                pass
+
+        return user_privilege
+
+    def has_privilege(self, target_privilege):
+        privileges = tuple(p for (p, _) in PRIVILEGE_LEVELS)
+        user_privilege = privileges.index(self.get_privilege())
+
+        try:
+            return privileges.index(target_privilege) <= user_privilege
+        except ValueError:
+            return False
+
+
+class GroupPrivilege(models.Model):
+    group = models.OneToOneField( \
+        Group, primary_key=True, on_delete=models.CASCADE)
+    privilege_level = models.CharField( \
+        default=None, null=True, max_length=6, choices=PRIVILEGE_LEVELS)
+
+    def __str__(self):
+        return '%s: %s' % (
+            self.group.name,
+            dict(PRIVILEGE_LEVELS)[self.privilege_level],
+        )
