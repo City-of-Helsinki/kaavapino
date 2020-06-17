74f5122ddc33d4de74becfa0cf2b1e73d63ceb6f
Nuppu Eskelinen <nuppu.eskelinen@nidos.fi>
2020-06-17 18:46:35 +0300

    Update user staff status based on privilege

--- users/signals/handlers.py
+++ users/signals/handlers.py
@@ -7,7 +7,13 @@ from users.models import User, GroupPrivilege
 
 @receiver(post_save, sender=User)
 def add_admin_group_post_user_creation(sender, instance, created, *args, **kwargs):
-    if created and instance.is_superuser:
+    if created and instance.is_staff:
         admin_group = Group.objects.filter(groupprivilege__privilege_level='admin').first()
         if admin_group:
             instance.groups.add(admin_group)
+    elif not instance.is_staff and instance.has_privilege('admin'):
+        instance.is_staff = True
+        instance.save()
+    elif instance.is_staff and not instance.has_privilege('admin'):
+        instance.is_staff = False
+        instance.save()
