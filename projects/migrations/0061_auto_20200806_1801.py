651d6d2637f7c354890aacc560c6ad2e8bdfa487
Nuppu Eskelinen <nuppu.eskelinen@nidos.fi>
2020-08-14 16:43:27 +0300

    Rename migration

--- projects/migrations/0061_auto_20200806_1801.py
+++ projects/migrations/0061_auto_20200806_1801.py
@@ -8,7 +8,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projects', '0059_project_archived'),
+        ('projects', '0060_project_onhold'),
     ]
 
     operations = [
