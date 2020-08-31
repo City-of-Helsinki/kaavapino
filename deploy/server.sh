650e60e4a8794167595d204515ba659b9a5361bd
Tomi Suomela <tomi.suomela@nidos.fi>
2020-08-31 17:42:54 +0300

    uwsgi.ini added for testing better performance

--- deploy/server.sh
+++ deploy/server.sh
@@ -7,5 +7,12 @@ echo "NOTICE: Get static files for serving"
 echo "Applying database migrations"
 python ./manage.py migrate --noinput
 
-echo -e "NOTICE: Start the uwsgi web server \n static files require WWW_ROOT env var"
-exec uwsgi --http :8000 --wsgi-file deploy/wsgi.py --check-static $WWW_ROOT -b 32768
+# Have gzipped versions ready for direct serving by uwsgi
+gzip --keep --best --force --recursive /code/static/
+
+# Restart the application
+echo "Restarting the application..."
+touch deploy/uwsgi.ini
+
+#echo -e "NOTICE: Start the uwsgi web server \n static files require WWW_ROOT env var"
+#exec uwsgi --http :8000 --wsgi-file deploy/wsgi.py --check-static $WWW_ROOT -b 32768
