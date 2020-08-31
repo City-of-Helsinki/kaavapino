322ef9f354fd2751cbae231ae0158cd1e7ac4657
Kaarle Hovila <kaarle.hovila@unikie.com>
2020-08-31 22:18:43 +0300

    Fixing uwsgi starting

--- deploy/server.sh
+++ deploy/server.sh
@@ -10,9 +10,7 @@ python ./manage.py migrate --noinput
 # Have gzipped versions ready for direct serving by uwsgi
 gzip --keep --best --force --recursive /code/static/
 
-# Restart the application
-echo "Restarting the application..."
-touch deploy/uwsgi.ini
-
-#echo -e "NOTICE: Start the uwsgi web server \n static files require WWW_ROOT env var"
-#exec uwsgi --http :8000 --wsgi-file deploy/wsgi.py --check-static $WWW_ROOT -b 32768
+if test "$1" = "start_production"; then
+    echo "Starting production"
+    exec uwsgi --ini /code/deploy/uwsgi.ini
+fi
