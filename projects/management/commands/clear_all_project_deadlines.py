f8c813fb93d86a7eb3ad16212ab237e4c647bb2b
Nuppu Eskelinen <nuppu.eskelinen@nidos.fi>
2021-01-28 09:14:48 +0200

    Improve clear_all_project_deadlines command

--- projects/management/commands/clear_all_project_deadlines.py
+++ projects/management/commands/clear_all_project_deadlines.py
@@ -1,8 +1,10 @@
+from actstream import action
 from django.core.management.base import BaseCommand, CommandError
 from django.core.serializers.json import json
 from django.db import transaction
 
-from projects.models import Project
+from projects.models import Project, Deadline
+from projects.actions import verbs
 
 
 class Command(BaseCommand):
@@ -10,9 +12,14 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument("--id", nargs="?", type=int)
+        parser.add_argument(
+            "--overwrite_all", nargs="?", type=bool,
+            help="Overwrite all deadline-related attribute_data entries, even if the deadline is not included in the current subtype"
+        )
 
     def handle(self, *args, **options):
         project_id = options.get("id")
+        overwrite_all = options.get("overwrite_all")
 
         if project_id:
             try:
@@ -24,26 +31,40 @@ class Command(BaseCommand):
 
         for project in projects:
             with transaction.atomic():
-                cleared_attributes = {
-                    project_dl.deadline.attribute.identifier: None
-                    for project_dl in project.deadlines.all()
-                    if project_dl.deadline.attribute
-                }
-                project.update_attribute_data(cleared_attributes)
-                for key, val in cleared_attributes.items():
+                if overwrite_all:
+                    cleared_attributes = [
+                        deadline.attribute
+                        for deadline in Deadline.objects.all()
+                        if deadline.attribute and \
+                            project.attribute_data.get(deadline.attribute.identifier)
+                    ]
+                else:
+                    cleared_attributes = [
+                        project_dl.deadline.attribute
+                        for project_dl in project.deadlines.all()
+                        if project_dl.deadline.attribute
+                    ]
+
+                project.update_attribute_data({
+                    attr.identifier: None
+                    for attr in cleared_attributes
+                })
+                project.save()
+
+                for attribute in cleared_attributes:
                     old_value = json.loads(json.dumps(
-                        project.attribute_data.get(key),
+                        project.attribute_data.get(attribute.identifier),
                         default=str,
                     ))
-                    new_value = json.loads(json.dumps(val, default=str))
+                    new_value = None
 
                     if old_value != new_value:
                         action.send(
-                            user=project.user,
+                            project.user,
                             verb=verbs.UPDATED_ATTRIBUTE,
-                            action_object=deadline.attribute,
-                            target=self,
-                            attribute_identifier=deadline.attribute.identifier,
+                            action_object=attribute,
+                            target=project,
+                            attribute_identifier=attribute.identifier,
                             old_value=old_value,
                             new_value=new_value,
                         )
