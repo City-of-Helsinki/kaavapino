093c2eab1a69ae9ab0f033f7cfebc4dd5ce71154
Nuppu Eskelinen <nuppu.eskelinen@nidos.fi>
2021-05-26 10:15:04 +0300

    Do not validate URLs

--- projects/serializers/section.py
+++ projects/serializers/section.py
@@ -28,7 +28,7 @@ FIELD_TYPES = {
     Attribute.TYPE_LONG_STRING: serializers.CharField,
     Attribute.TYPE_RICH_TEXT: serializers.JSONField,
     Attribute.TYPE_RICH_TEXT_SHORT: serializers.JSONField,
-    Attribute.TYPE_LINK: serializers.URLField,
+    Attribute.TYPE_LINK: serializers.CharField,
     Attribute.TYPE_INTEGER: serializers.IntegerField,
     Attribute.TYPE_DECIMAL: serializers.DecimalField,
     Attribute.TYPE_BOOLEAN: serializers.NullBooleanField,
