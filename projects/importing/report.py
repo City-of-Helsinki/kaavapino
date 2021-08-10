58f54bdb7d7af03a6196aa4381e48d63603efed2
Nuppu Eskelinen <nuppu.eskelinen@nidos.fi>
2021-08-10 12:40:29 +0300

    Add report type importer

--- projects/importing/report.py
+++ projects/importing/report.py
@@ -1,24 +1,166 @@
-from projects.models import ProjectType, Report, Attribute, ReportColumn
+import logging
+import re
+from typing import Iterable, Sequence
+
+from openpyxl import load_workbook
+from django.db import transaction
+
+from projects.models import (
+    ProjectType,
+    Report,
+    Attribute,
+    CommonProjectPhase,
+    Report,
+    ReportColumn,
+    ReportColumnPostfix,
+)
+
+logger = logging.getLogger(__name__)
+
+# Sheets
+REPORT_SHEET_NAME = ""
+
+# Report sheet column titles
+REPORT_NAME = "raportin nimi"
+COLUMN_ATTRIBUTES = "kentät"
+COLUMN_CONDITIONS = "näyttöehto"
+COLUMN_POSTFIX = "loppuliite"
+COLUMN_INDEX = "sarakkeiden järjestys"
+COLUMN_TITLE = "sarakkeen otsikko"
+
+# Hard-coded for now because only one report type is
+# previewable at this point
+# TODO check its name
+PREVIEWABLE_REPORTS = []
+
+class ReportImporterException(Exception):
+    pass
 
 
-class ReportTypeCreator:
-    def run(self):
-        project_type, _ = ProjectType.objects.get_or_create(name="asemakaava")
-        all_attributes, _ = Report.objects.update_or_create(
-            name="Kaikki kentät",
-            project_type=project_type,
-            defaults={
-                "is_admin_report": True,
-                "show_name": True,
-                "show_created_at": True,
-                "show_modified_at": True,
-                "show_user": True,
-                "show_phase": True,
-                "show_subtype": True,
-            },
+class ReportImporter:
+    """Import reports from an Excel file"""
+    def __init__(self, options=None):
+        self.options = options
+        self.workbook = None
+
+    def _open_workbook(self, filename):
+        try:
+            return load_workbook(filename, read_only=True, data_only=True)
+        except FileNotFoundError as e:
+            raise ReportImporterException(e)
+
+    def _extract_data_from_workbook(self, workbook):
+        try:
+            report_sheet = workbook[REPORT_SHEET_NAME]
+        except KeyError as e:
+            raise ReportImporterException(e)
+
+        report_sheet_a1_value = report_sheet["A1"].value
+        invalid_sheet_exception = ReportImporterException(
+            "This does not seem to be a valid report type sheet."
         )
-        all_attributes.report_attributes.all().delete()
+        try:
+            if report_sheet_a1_value.lower() != REPORT_NAME:
+                raise invalid_sheet_exception
+        except AttributeError:
+            raise invalid_sheet_exception
 
-        for attribute in Attribute.objects.report_friendly():
-            column = ReportColumn.objects.create(report=all_attributes)
-            columns.attributes.set([attribute])
+        return self._rows_for_report_sheet(report_sheet)
+
+    def _rows_for_report_sheet(self, sheet):
+        data = []
+
+        for row in list(sheet.iter_rows()):
+            if not row[1].value:
+                break
+            data.append([col.value for col in row])
+
+        return data
+
+    def _set_row_indexes(self, header_row: Iterable[str]):
+        """Determine index number for all columns."""
+        self.column_index: dict = {}
+
+        for index, column in enumerate(header_row):
+            if column:
+                self.column_index[column.lower()] = index
+
+    def _check_if_row_valid(self, row: Sequence) -> bool:
+        """Check if the row has all required data."""
+        try:
+            assert(row[self.column_index[REPORT_NAME]])
+        except AssertionError:
+            logger.info(f"Invalid row missing report name not imported.")
+            return False
+
+        return True
+
+    def _create_report_columns(self, rows):
+        project_type, _ = ProjectType.objects.get_or_create(name="asemakaava")
+        ReportColumn.objects.all().delete()
+        ReportColumnPostfix.objects.all().delete()
+
+        for row in rows:
+            report_name = row[self.column_index[REPORT_NAME]]
+            report, _ = Report.objects.get_or_create(
+                name=report_name,
+                project_type=project_type,
+                defaults={
+                    "is_admin_report": True,
+                    "show_created_at": True,
+                    "show_modified_at": True,
+                    "previewable": report_name in PREVIEWABLE_REPORTS,
+                }
+            )
+
+            column = ReportColumn.objects.create(
+                report=report,
+                title=row[self.column_index[COLUMN_TITLE]],
+                index=row[self.column_index[COLUMN_INDEX]] or 0,
+            )
+
+            attributes = row[self.column_index[COLUMN_ATTRIBUTES]].split(",")
+            conditions = row[self.column_index[COLUMN_CONDITIONS]].split(",")
+
+            column.attributes.set(
+                Attribute.objects.filter(identifier__in=attributes)
+            )
+            column.conditions.set(
+                Attribute.objects.filter(identifier__in=conditions)
+            )
+
+            postfixes = zip(
+                re.findall(
+                    r"\[([X,S,M,L]*)\]",
+                    row[self.column_index[COLUMN_POSTFIX]],
+                ),
+                re.findall(
+                    r'"([^"]*)"',
+                    row[self.column_index[COLUMN_POSTFIX]],
+                ),
+            )
+            for phases, formatting in postfixes:
+                ReportColumnPostfix.objects.create(
+                    report_column=column,
+                    phases=CommonProjectPhase.objects.filter(
+                        name__in=phases.split(","),
+                    ),
+                    formatting=formatting,
+                )
+
+    @transaction.atomic
+    def run(self):
+        filename = self.options.get("filename")
+        logger.info(f"Importing report types and columns from {filename}")
+
+        self.workbook = self._open_workbook(filename)
+
+        data_rows = \
+            self._extract_data_from_workbook(self.workbook)
+        header_row = data_rows.pop(0)
+        self._set_row_indexes(header_row)
+        data_rows = [
+            row for row in data_rows
+            if self._check_if_row_valid(row)
+        ]
+        self._create_report_columns(data_rows)
