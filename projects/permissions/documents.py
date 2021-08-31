764b86dfa33d4e11fa14a3794dfb16de063fb0a3
Nuppu Eskelinen <nuppu.eskelinen@nidos.fi>
2021-08-31 10:42:08 +0300

    Tweak document permissions

--- projects/permissions/documents.py
+++ projects/permissions/documents.py
@@ -3,10 +3,13 @@ from rest_framework import permissions
 
 class DocumentPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
+        preview = request.query_params.get("preview") in ("true", "True", "1")
         project = view.get_project()
         user = request.user
 
-        if project.user == user or user.has_privilege('admin'):
-            return True
-
-        return False
+        if preview and view.action == "retrieve":
+            return user.has_privilege('edit')
+        elif view.action == "list":
+            return user.has_privilege('edit')
+        else:
+            return project.user == user or user.has_privilege('admin')
