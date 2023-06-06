1e137d582828723344961999061d82e08eec0a07
HenryVisuri <henry.visuri@haallas.com>
2023-06-06 14:52:59 +0300

    Fixed fieldset value delivery to frontend when locking an attribute

--- projects/serializers/projectschema.py
+++ projects/serializers/projectschema.py
@@ -169,10 +169,11 @@ class AttributeLockSerializer(serializers.Serializer):
                 if f_data and isinstance(f_data, list) and len(f_data) > 0:
                     f_index_data = f_data[int(attribute_lock_data.get("fieldset_attribute_index"))]
                     return f_index_data.get(attribute_lock_data.get("attribute_identifier"))
-                return None
-            return attribute_lock.project.attribute_data[attribute_lock_data.get("attribute_identifier")]
+            else:
+                return attribute_lock.project.attribute_data[attribute_lock_data.get("attribute_identifier")]
         except KeyError:  # Attribute doesn't exist in projects attribute_data
-            return None
+            pass
+        return None
 
     def get_user_name(self, attribute_lock):
         return f'{attribute_lock.user.first_name} {attribute_lock.user.last_name}'
