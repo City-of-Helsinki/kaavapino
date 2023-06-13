13dcbea0861f0da17f7dd2eac77800e483350501
HenryVisuri <henry.visuri@haallas.com>
2023-06-13 11:27:47 +0300

    Rename roles and subroles to field_roles and field_subroles to keep naming similar with AttributeSchemaSerializer

--- projects/serializers/projectschema.py
+++ projects/serializers/projectschema.py
@@ -129,8 +129,8 @@ class AutofillRuleSerializer(serializers.Serializer):
 class SimpleAttributeSerializer(serializers.Serializer):
     label = serializers.CharField(source="name")
     name = serializers.CharField(source="identifier")
-    roles = serializers.CharField(source="field_roles")
-    subroles = serializers.CharField(source="field_subroles")
+    field_roles = serializers.CharField()
+    field_subroles = serializers.CharField()
 
 
 class AttributeLockSerializer(serializers.Serializer):
