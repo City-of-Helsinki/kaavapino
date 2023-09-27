ac03284d6eea0b400bfacf0e85104da44e5fbf8f
HenryVisuri <henry.visuri@haallas.com>
2023-09-27 15:41:38 +0300

    Modify get_field_data to return full data for fieldset index

--- projects/serializers/projectschema.py
+++ projects/serializers/projectschema.py
@@ -171,8 +171,7 @@ class AttributeLockSerializer(serializers.Serializer):
             if attribute_lock_data.get("fieldset_attribute_identifier") is not None:
                 f_data = attribute_lock.project.attribute_data[attribute_lock_data.get("fieldset_attribute_identifier")]
                 if f_data and isinstance(f_data, list) and len(f_data) > 0:
-                    f_index_data = f_data[int(attribute_lock_data.get("fieldset_attribute_index"))]
-                    return f_index_data.get(attribute_lock_data.get("attribute_identifier"))
+                    return f_data[int(attribute_lock_data.get("fieldset_attribute_index"))]
             else:
                 return attribute_lock.project.attribute_data[attribute_lock_data.get("attribute_identifier")]
         except KeyError:  # Attribute doesn't exist in projects attribute_data
