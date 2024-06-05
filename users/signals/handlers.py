e5e07fe8806be8777f1a64a91b263c9508137632
Eemeli Kukkonen <eemeli.kukkonen@siili.com>
2024-06-05 10:47:31 +0300

    Handle case where multiple existing users are in development db

--- users/signals/handlers.py
+++ users/signals/handlers.py
@@ -76,18 +76,17 @@ def update_user_ad_data(sender, instance, *args, **kwargs):
 
 @receiver(pre_save, sender=User)
 def update_existing_user(sender, instance, raw, using, update_fields, *args, **kwargs):
-    try:
-        old_user = User.objects.get(first_name=instance.first_name,
-                                last_name=instance.last_name,
-                                email=instance.email,
-                                )
-        if instance.id and old_user.id != instance.id:
-            new_groups = (old_user.groups.all() |  instance.groups.all())
-            instance.groups.set(new_groups)
-            instance.additional_groups.set(old_user.additional_groups.all() | instance.additional_groups.all())
-        instance.id = old_user.id
-    except User.DoesNotExist:
-            pass  # Old user not found
+    old_users = User.objects.filter(first_name=instance.first_name,
+                            last_name=instance.last_name,
+                            email=instance.email,
+                            )
+    if not old_users:
+        return
+    old_user = old_users[0]
+    if old_user and instance.id and old_user.id != instance.id:
+        instance.groups.set(old_user.groups.all() |  instance.groups.all())
+        instance.additional_groups.set(old_user.additional_groups.all() | instance.additional_groups.all())
+    instance.id = old_user.id
 
 @receiver(m2m_changed, sender=User.additional_groups.through)
 def handle_additional_groups(sender, instance, action, pk_set, **kwargs):
