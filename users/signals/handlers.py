ade4a15e234d0a3a2e05dfeb5606401bb3368b77
Eemeli Kukkonen <eemeli.kukkonen@siili.com>
2024-08-26 13:43:32 +0300

    KAAV-1642 Add migration for django-admin user

--- users/signals/handlers.py
+++ users/signals/handlers.py
@@ -76,11 +76,17 @@ def update_user_ad_data(sender, instance, *args, **kwargs):
 
 @receiver(pre_save, sender=User)
 def update_existing_user(sender, instance, raw, using, update_fields, *args, **kwargs):
-    old_users = User.objects.filter(first_name=instance.first_name,
-                            last_name=instance.last_name,
-                            email=instance.email,
-                            username__startswith="u-"
-                            )
+    try:
+        if not instance.username.startswith('u-'):
+            old_users = User.objects.filter(username=instance.username)
+    except AttributeError:
+        pass
+    if not old_users:
+        old_users = User.objects.filter(first_name=instance.first_name,
+                                last_name=instance.last_name,
+                                email=instance.email,
+                                username__startswith="u-"
+                                )
     if not old_users:
         return
     old_user = old_users[0]
