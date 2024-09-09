0aa6ed70b13f77552361f7fe97a0d895df3026a1
Eemeli Kukkonen <eemeli.kukkonen@siili.com>
2024-09-09 16:49:25 +0300

    KAPI-48 Fix missing AD info on attribute_data request

--- projects/views.py
+++ projects/views.py
@@ -33,7 +33,7 @@ from rest_framework_extensions.mixins import NestedViewSetMixin
 
 from projects.exporting.document import render_template
 from projects.exporting.report import render_report_to_response
-from projects.helpers import DOCUMENT_CONTENT_TYPES, get_file_type, TRUE, get_attribute_lock_data
+from projects.helpers import DOCUMENT_CONTENT_TYPES, get_file_type, TRUE, get_attribute_lock_data, set_ad_data_in_attribute_data
 from projects.importing import AttributeImporter, AttributeUpdater
 from projects.models import (
     FieldComment,
@@ -891,7 +891,9 @@ class ProjectViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
             project = Project.objects.get(name=project_name)
 
             if attribute_identifier is not None:
-                return Response({attribute_identifier: project.attribute_data[attribute_identifier]})
+                data = {attribute_identifier: project.attribute_data[attribute_identifier]}
+                set_ad_data_in_attribute_data(data)
+                return Response(data)
 
             return Response({"attribute_data": project.attribute_data})
         except Project.DoesNotExist as exc:
