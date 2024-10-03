4188f04c235114fcf0a2b8097eada8f15a81afae
HenryVisuri <henry.visuri@haallas.com>
2024-10-03 11:05:29 +0300

    Added management script to update project deadlines

---
+++ projects/management/commands/update_project_deadlines.py
@@ -0,0 +1,31 @@
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from projects.models import Project
+
+import logging
+
+log = logging.getLogger(__name__)
+
+class Command(BaseCommand):
+    help = "Update project deadlines"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--id", nargs="?", type=int)
+
+    def handle(self, *args, **options):
+        project_id = options.get("id")
+
+        if project_id:
+            try:
+                projects = [Project.objects.get(pk=project_id)]
+            except Project.DoesNotExist:
+                projects = Project.objects.all()
+        else:
+            projects = Project.objects.all()
+
+        for project in projects:
+            log.info(f'Updating project "{project.name}" deadlines')
+            with transaction.atomic():
+                project.update_deadlines()
+                project.save()
