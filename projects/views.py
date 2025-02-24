ce93eef6d4f9251b411a2c4e49f446fd4acee1ca
Eemeli Kukkonen <eemeli.kukkonen@siili.com>
2025-02-24 15:37:41 +0200

    Remove debug prints and time.time() calls

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,6 @@
 import pytz
 import csv
 from datetime import datetime, timedelta, date
-import time
 import logging
 
 from django.contrib.postgres.search import SearchVector
@@ -109,7 +108,6 @@ from projects.serializers.utils import get_dl_vis_bool_name, should_display_dead
 from sitecontent.models import ListViewAttributeColumn
 from projects.clamav import clamav_client, FileScanException, FileInfectedException
 
-import time
 
 log = logging.getLogger(__name__)
 
@@ -209,7 +207,6 @@ class ProjectViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
         return ProjectSerializer
 
     def get_queryset(self):
-        start = time.time()
         user = self.request.user
         queryset = self.queryset
         if self.request.method in ['PUT', 'PATCH']:
@@ -247,7 +244,6 @@ class ProjectViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
                 queryset = queryset.filter(onhold=True)
             elif status == "archived":
                 queryset = queryset.filter(archived=True)
-        print("getting queryset part took (s): " + str(time.time() - start))
         return queryset
 
     def _string_filter_to_list(self, filter_string):
@@ -931,13 +927,8 @@ class ProjectViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
 
         # Run update in 'ghost' mode where no changes are applied to database but result is returned
         with transaction.atomic():
-            start = time.time()
             result = super().update(request, *args, **kwargs)
-            result_end = time.time()
             transaction.set_rollback(True)
-            rollback_end = time.time()
-            print("Time elapsed during calculating:" + str(result_end - start))
-            print("Time elapsed during rollback: "+ str(rollback_end - result_end))
             return result
 
 
