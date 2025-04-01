dd26b61367fbae5bb186e0a569677d226685453f
HenryVisuri <henry.visuri@haallas.com>
2025-04-01 14:28:42 +0300

    KAAV-2613 Kaavoitus-API project data request implementations

--- projects/views.py
+++ projects/views.py
@@ -32,7 +32,15 @@ from rest_framework_extensions.mixins import NestedViewSetMixin
 
 from projects.exporting.document import render_template
 from projects.exporting.report import render_report_to_response
-from projects.helpers import DOCUMENT_CONTENT_TYPES, get_file_type, TRUE, get_attribute_lock_data
+from projects.helpers import (
+    DOCUMENT_CONTENT_TYPES,
+    get_file_type,
+    TRUE,
+    get_attribute_lock_data,
+    set_ad_data_in_attribute_data,
+    get_in_personnel_data,
+    set_geoserver_data_in_attribute_data,
+)
 from projects.importing import AttributeImporter, AttributeUpdater
 from projects.models import (
     FieldComment,
@@ -359,6 +367,65 @@ class ProjectViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     )
     def simple(self, request, pk):
         return Response(SimpleProjectSerializer(self.get_object()).data)
+
+    @extend_schema(
+        responses={
+            200: OpenApiTypes.STR,
+            400: OpenApiTypes.STR,
+            401: OpenApiTypes.STR
+        }
+    )
+    @action(
+        methods=["get"],
+        detail=True,
+        permission_classes=[IsAuthenticated, ProjectPermissions],
+    )
+    def simple_filtered(self, request, pk):  # Filter returned Attributes by Attribute.api_visibility
+        project = ProjectSerializer(self.get_object(), context={"request": request}).data
+        attributes = {attr.identifier: attr for attr in Attribute.objects.all()}
+        response = {
+            "project_name": project.get("name"),
+        }
+        attribute_data = project.get("attribute_data", None)
+        if not attribute_data:
+            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+        set_ad_data_in_attribute_data(attribute_data)
+        set_geoserver_data_in_attribute_data(attribute_data)
+
+        for key, value in attribute_data.items():
+            attribute = attributes.get(key)
+            if not attribute or not attribute.api_visibility:
+                continue
+
+            if not value:
+                continue
+
+            if attribute.value_type == Attribute.TYPE_FIELDSET:
+                fieldset = []
+                for entry in value:  # fieldset
+                    fieldset_obj = {}
+                    for k, v in entry.items():
+                        fieldset_attr = attributes.get(k, None)
+                        if not fieldset_attr or not fieldset_attr.api_visibility:
+                            continue
+                        if fieldset_attr.value_type == Attribute.TYPE_PERSONNEL:
+                            v = get_in_personnel_data(v, "name", False)
+                        elif fieldset_attr.value_type in [Attribute.TYPE_RICH_TEXT, Attribute.TYPE_RICH_TEXT_SHORT]:
+                            v = "".join([item["insert"] for item in v["ops"]]).strip()
+                        fieldset_obj[k] = v
+                    if fieldset_obj:
+                        fieldset.append(fieldset_obj)
+                if fieldset:
+                    response[key] = fieldset
+            elif attribute.value_type == Attribute.TYPE_USER:
+                response[key] = get_in_personnel_data(value, "name", True)
+            elif attribute.value_type in [Attribute.TYPE_RICH_TEXT, Attribute.TYPE_RICH_TEXT_SHORT]:
+                response[key] = "".join([item["insert"] for item in value["ops"]]).strip()
+            else:
+                response[key] = value
+
+        return Response(response)
 
     @extend_schema(
         request=ProjectFileSerializer,
