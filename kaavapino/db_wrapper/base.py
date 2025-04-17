1cf5fb901da1608c7a157fe271f4a81033aa737a
Eemeli Kukkonen <eemeli.kukkonen@siili.com>
2025-04-17 09:46:04 +0300

    Add explanation in comment

--- kaavapino/db_wrapper/base.py
+++ kaavapino/db_wrapper/base.py
@@ -1,3 +1,9 @@
+# This wrapper is copied from django\contrib\gis\db\backends\postgis\base.py
+# and is supposed to work exactly as the default postgis backend. The only change is
+# an additional check for if postgis extension is already installed (was causing errors in migrate)
+# This same fix is included in Django versions 4.2 and newer. If upgrading Django to 4.2+, this file
+# should be deleted and the default database backend used.
+
 from django.db.backends.base.base import NO_DB_ALIAS
 from django.db.backends.postgresql.base import (
     DatabaseWrapper as Psycopg2DatabaseWrapper,
@@ -21,7 +27,7 @@ class DatabaseWrapper(Psycopg2DatabaseWrapper):
 
     def prepare_database(self):
         super().prepare_database()
-        # ONLY CHANGE TO ORIGINAL: Return if extension already installed
+        # CHANGE TO ORIGINAL: Return if extension already installed
         with self.cursor() as cursor:
             cursor.execute("SELECT 1 FROM pg_extension WHERE extname = %s", ["postgis"])
             if bool(cursor.fetchone()):
